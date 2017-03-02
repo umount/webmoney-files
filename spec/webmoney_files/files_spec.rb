100d0eeb43653b2c1864c6165e75707a5841d3f0
Denis Sobolev <dns.sobol@gmail.com>
2017-03-02 19:41:02 +0300

    test: add fixture file and file spec

---
+++ spec/webmoney_files/files_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe WebmoneyFiles, :webmoney do
+  let(:storage) {
+    WebmoneyFiles.new({
+      host: 'https://files.dev.local',
+      webmoney_instance: @webmoney
+    })
+  }
+
+  it 'upload file by path' do
+    file_path = File.expand_path('../fixture/webmoney_icon.png', __FILE__)
+    response = storage.file.upload(file_path)
+
+    expect(response).to include('object')
+    expect(response['object']).to include('id')
+  end
+end

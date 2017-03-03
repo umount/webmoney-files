beeadebc7e80cd277b8a7920b2bfe77e621d35a8
Denis Sobolev <dns.sobol@gmail.com>
2017-03-03 12:28:25 +0300

    test: add delete folder spec

--- spec/webmoney_files/folders_spec.rb
+++ spec/webmoney_files/folders_spec.rb
@@ -15,16 +15,26 @@ describe WebmoneyFiles, :webmoney do
     expect(response['object']).to include('id')
   end
 
+  it 'delete folder' do
+    folder = storage.folder.create('test_folder')
+
+    response = storage.folder.delete(folder['object']['id'])
+    expect(response).to eq(true)
+  end
+
   it 'create folder and upload file' do
     folder = storage.folder.create('test')
 
     file_path = File.expand_path('../../fixture/webmoney_icon.png', __FILE__)
-    response = storage.file.upload(file_path, {
+    file = storage.file.upload(file_path, {
       parent_id: folder['object']['id']
     })
 
-    expect(response).to include('object')
-    expect(response['object']).to include('id')
-    expect(response['object']['parent_id']).to eq(folder['object']['id'])
+    expect(file).to include('object')
+    expect(file['object']).to include('id')
+    expect(file['object']['parent_id']).to eq(folder['object']['id'])
+
+    response = storage.folder.delete(folder['object']['id'])
+    expect(response).to eq(true)
   end
 end
